6e765da78fbfd8ad460f600f1b08be2931272ece
franzheinau <rifaldi0823666@gamail.com>
2026-04-27 10:40:27 +0800

    hai

--- frontend/src/context/MusicContext.jsx
+++ frontend/src/context/MusicContext.jsx
@@ -3,7 +3,7 @@ import { useMusic } from "../hooks/useMusic";
 
 const MusicCtx = createContext(null);
 
-const MUSIC_URL = "https://files.catbox.moe/2zeyfo.mp3"; // ← ganti ini
+const MUSIC_URL = "/Pixel.mp3"; // ← ganti ini
 
 export function MusicProvider({ children }) {
   const music = useMusic(MUSIC_URL, { volume: 0.35 });
